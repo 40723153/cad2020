de9ca4a04d04a597f6302055e2ceb294c09e7fb7
40723153 <40723153@gm.nfu.edu.tw>
2021-01-07 14:59:38 +0800

    w17update

--- init.py
+++ init.py
@@ -10,7 +10,9 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "40723153 cad2020"
+    site_title = "CMSiMDE"
+    ip = "127.0.0.1"
+    port = 9443
     def __init__(self):
         # hope to create downloads and images directories　
         if not os.path.isdir(_curdir + "/downloads"):
